2c58a238284e709a589f1ddc5c0c96620ef03f4e
Leila <leila.cheshmi@gmail.com>
2024-12-14 11:41:22 -0500

    add file

---
+++ doubly_linked_lint_data.py
@@ -0,0 +1,40 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def append(self, data):
+        pass
+
+    def pre_append(self, data):
+        pass
+
+    def insert_at_position(self, data, position):
+        pass
+
+    def search(self, data):
+        pass
+
+    def delete(self, data):
+        pass
+
+    def is_empty(self):
+        return self.head is None
+
+    def display(self):
+        pass
+
+
+if __name__ == "__main__":
+    dll = DoublyLinkedList()
+
+
+
