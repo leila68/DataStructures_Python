a9a0d137fefbf5163cdcb408a8cafcdbd2c90559
Leila <leila.cheshmi@gmail.com>
2024-12-14 14:25:31 -0500

    add insert func

--- doubly_linked_lint_data.py
+++ doubly_linked_lint_data.py
@@ -11,14 +11,52 @@ class DoublyLinkedList:
         self.head = None
         self.size = 0
 
-    def append(self, data):
-        pass
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.size += 1
+            return True
+        new_node.next = self.head
+        self.head.prev = new_node
+        self.head = new_node
+        self.size += 1
 
-    def pre_append(self, data):
-        pass
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.size += 1
+            return self.size
+        current = self.head
+        while current.next:
+            current = current.next
+        new_node.prev = current
+        current.next = new_node
+        self.size += 1
 
     def insert_at_position(self, data, position):
-        pass
+        if position > self.size:
+            print(f"Attempted to insert at position {position}, but it's invalid.")
+            raise IndexError("Invalid position")
+
+        new_node = Node(data)
+        current = self.head
+        previous = current
+
+        pos = 0
+        while current:
+            if pos == position:
+                current.prev = new_node
+                new_node.next = current
+                previous.next = new_node
+                new_node.prev = previous
+                self.size += 1
+                return True
+            previous = current
+            current = current.next
+            pos += 1
+        return False
 
     def search(self, data):
         pass
@@ -30,11 +68,27 @@ class DoublyLinkedList:
         return self.head is None
 
     def display(self):
-        pass
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print('\n')
 
 
 if __name__ == "__main__":
     dll = DoublyLinkedList()
+    dll.insert_at_beginning(10)
+    # dll.insert_at_beginning(11)
+    dll.display()
+    dll.insert_at_end(20)
+    dll.insert_at_end(30)
+    dll.insert_at_end(40)
+    dll.insert_at_end(50)
+    dll.display()
+    dll.insert_at_position(44, 4)
+    dll.display()
+    print(dll.size)
+
 
 
 
