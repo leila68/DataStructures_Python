64ad8119c82719310ae445761573351925ffbcc5
Leila <leila.cheshmi@gmail.com>
2024-12-16 01:25:09 -0500

    add file

---
+++ ten_functions.py
@@ -0,0 +1,52 @@
+# 1
+def largest_three_numbers():
+    pass
+
+
+# 2
+def is_prime(num: int) -> bool:
+    pass
+
+
+# 3
+def compound_interest():
+    pass
+
+
+# 4
+def swap_two_numbers(num1, num2):
+    pass
+
+
+# 5
+def replace_zero():
+    pass
+
+
+# 6
+def binary_to_decimal():
+    pass
+
+
+#7
+def is_leap_year():
+    pass
+
+
+# 8
+def factorial():
+    pass
+
+
+# 9
+def is_armstrong():
+    pass
+
+
+# 10
+def find_roots():
+    pass
+
+
+if __name__ == '__main__':
+    pass
