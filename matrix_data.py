56baf2e8919c9e6c794dd0310a3acd4a6bf5baa4
Leila <leila.cheshmi@gmail.com>
2024-12-21 22:46:37 -0500

    update file

--- matrix_data.py
+++ matrix_data.py
@@ -2,52 +2,41 @@ class Matrix:
     def __init__(self, mtx):
         self.mtx = mtx
 
-    def matrix_dim(self):
-        row = len(self.mtx)
-        col = len(self.mtx[0])
+    def matrix_dim(self, mtx=None):
+        if mtx is None:
+            mtx = self.mtx
 
-        return row, col
-
-    def display(self):
-        row, col = self.matrix_dim()
-        for i in range(row):
-            for j in range(col):
-                print(self.mtx[i][j], end=' ')
-            print('')
-
-    def new_matrix_dim(self, mtx):
         row = len(mtx)
         col = len(mtx[0])
         return row, col
 
-    def new_display(self, mtx):
-        row, col = self.new_matrix_dim(mtx)
-        for i in range(row):
-            for j in range(col):
-                print(mtx[i][j], end=' ')
-            print('')
+    def display(self, mtx=None):
+        if mtx is None:
+            mtx = self.mtx
+        for row in mtx:
+            print(' '.join(map(str, row)))
+        print()  # Add a blank line for better readability.
 
     def matrix_transpose(self):
         row, col = self.matrix_dim()
-        trans_mtx = [[0] * row for _ in range(col)]
-        # transposed = []
-        # for _ in range(cols):
-        #     transposed.append([0] * rows)
-
-        for i in range(row):
-            for j in range(col):
-                trans_mtx[j][i] = self.mtx[i][j]
-        self.new_display(trans_mtx)
+        # Use list comprehension to transpose the matrix
+        transposed = [[self.mtx[i][j] for i in range(row)] for j in range(col)]
+        return transposed
 
 
 if __name__ == '__main__':
-    matrix = [[1, 2, 3, 4],
-              [5, 6, 7, 8],
-              [9, 10, 11, 12]]
-    mtx = Matrix(matrix)
+    matrix = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12]
+    ]
 
-    mtx.display()
-    print(mtx.matrix_dim())
-    mtx.matrix_transpose()
+    mt = Matrix(matrix)
+    print("Matrix Dimensions:", mt.matrix_dim())
+    print("Original Matrix:")
+    mt.display()
 
-
+    print("Transposed Matrix:")
+    transposed = mt.matrix_transpose()
+    print("Matrix Dimensions:", mt.matrix_dim(transposed))
+    mt.display(transposed)
