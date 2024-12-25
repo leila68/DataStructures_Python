403cb5e2095eaf6d523d9bd5753d7b25a525af2f
Leila <leila.cheshmi@gmail.com>
2024-12-24 22:41:23 -0500

    add bubble_sort

--- sort_algorithms.py
+++ sort_algorithms.py
@@ -16,11 +16,21 @@ class Search:
                 left = mid + 1
         return 0
 
+    def bubble_sort(self):
+        for i in range(self.size-1):
+            for j in range(self.size-1-i):
+                if self.data[j] > self.data[j + 1]:
+                    self.data[j], self.data[j+1] = self.data[j+1], self.data[j]
+        return data
+
 
 if __name__ == '__main__':
-    data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    # data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    data = [100, 60, 40, 70, 50, 20, 80, 90, 10]
     sr = Search(data)
+    print(sr.bubble_sort())
     print(sr.binary_search(90))
+
 
 
 
