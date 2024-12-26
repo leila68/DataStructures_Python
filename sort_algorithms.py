57c97c3baea23c8a704e757ef6a0d3df0942bc9c
Leila <leila.cheshmi@gmail.com>
2024-12-25 21:37:23 -0500

    add insertion_sort

--- sort_algorithms.py
+++ sort_algorithms.py
@@ -17,19 +17,52 @@ class Search:
         return 0
 
     def bubble_sort(self):
+        # time complexity: O(n^2)
         for i in range(self.size-1):
             for j in range(self.size-1-i):
                 if self.data[j] > self.data[j + 1]:
                     self.data[j], self.data[j+1] = self.data[j+1], self.data[j]
         return data
 
+    def selection_sort(self):
+        # time complexity: O(n^2)
+        end = self.size-1
+        while end >= 0:
+            max_idx = 0
+            for i in range(end+1):
+                if self.data[i] > self.data[max_idx]:
+                    max_idx = i
+            self.data[max_idx], self.data[end] = self.data[end], self.data[max_idx]
+            end -= 1
+
+        return self.data
+
+    def insertion_sort(self):
+        # time complexity: O(n^2)
+        for i in range(1, self.size):
+            key = self.data[i]
+            j = i-1
+            while j >= 0 and self.data[j] > key:
+                self.data[j+1] = self.data[j]
+                j -= 1
+            self.data[j+1] = key
+        return self.data
+
+    def merge_sort(self):
+        pass
+
+    def quick_sort(self):
+        pass
+
 
 if __name__ == '__main__':
     # data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     data = [100, 60, 40, 70, 50, 20, 80, 90, 10]
     sr = Search(data)
-    print(sr.bubble_sort())
-    print(sr.binary_search(90))
+    # print(sr.bubble_sort())
+    # print(sr.binary_search(90))
+    # print(sr.selection_sort())
+    print(sr.insertion_sort())
 
 
 
